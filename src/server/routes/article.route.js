7c51685e00c9682d0137ebb2bc19f986149d6233
MiaChen <miaqwcc@gmail.com>
2021-12-14 11:07:51 +0800

    加入mysql post測試新增一筆文章

---
+++ src/server/routes/article.route.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import articleCtrl from '../controllers/article.controller';
+
+const router = express.Router();
+router.route('/').post(articleCtrl.articlePost); /** 新增 Article 值組 */
+
+export default router;
--- src/server/routes/index.route.js
+++ src/server/routes/index.route.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import config from '../../config/config';
+import article from './article.route';
 const router = express.Router();
+import bodyParser from 'body-parser';
 
-router.get('/', function(req, res) {
+router.get('/', (req, res) => {
 	res.send(`This is api page http://localhost:${config.port}/api`);
 });
-router.get('/sqlTest', function(req, res) {});
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use('/article', article);
 export default router;
